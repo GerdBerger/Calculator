ad7002c879b5386c0dd0393348bcda9777f8a66b
GerdBerger <gerd.berger@student.tugraz.at>
2017-03-15 15:08:54 +0100

    [GB] created CalculatorUnitTest and Calcualtions.java

---
+++ Calculator/app/src/main/java/at/sw2017/calculator/Calculations.java
@@ -0,0 +1,27 @@
+package at.sw2017.calculator;
+
+/**
+ * Created by Gerd on 15.03.2017.
+ */
+
+public class Calculations {
+
+    private Calculations() {
+    }
+
+    static int doAddition(int firstNumber, int secondNumber) {
+        return 0;
+    }
+
+    static int doSubtraction(int firstNumber, int secondNumber) {
+        return 0;
+    }
+
+    static int doMultiplication(int firstNumber, int secondNumber) {
+        return 0;
+    }
+
+    static int doDivision(int firstNumber, int secondNumber) {
+        return 0;
+    }
+}
